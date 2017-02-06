f57c8b530164e2a58fdc8e57ea85e15b4ac0b46f
ROB-PC3\administrator75 <rob@bloodandtreasure.com>
2017-02-06 16:22:43 +0000

    no message

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [DataTables](https://www.datatables.net) is a jQuery plugin for intelligent HTML tables. Next to adding dynamic elements to the table, it also has great supports for on-demand data fetching and server-side processing. The _cakephp-datatables_ plugin makes it easy to use the functionality DataTables provides in your CakePHP 3 application. It consists of a helper to add DataTables to your view and a Component to transparently process AJAX requests made by DataTables.
 
-This branch brings integrates with the powerful Angular.js. The framework uses data binding and dependency injection meaning that minimal HTML mark-up is all that is required to generate feature rich DataTables. This implmentation removes any need for CakePHP helpers since Angular handles the front-end.
+This branch further harnesses CakePHP-DataTables by making use of Angular.js which uses data binding and dependency injection meaning that only minimal HTML mark-up required to generate feature rich DataTables. This implmentation removes any need for CakePHP helpers since Angular.js handles the front-end generation.
 
 DOCUMENTATION IS UNDER DEVELOPMENT AND INCOMPLETE
 
--- src/Controller/Component/DataTablesComponent.php
+++ src/Controller/Component/DataTablesComponent.php
@@ -1,35 +1,28 @@
 <?php
 namespace DataTables\Controller\Component;
-
 use Cake\Controller\Component;
 use Cake\ORM\TableRegistry;
-
 /**
  * DataTables component
  */
 class DataTablesComponent extends Component
 {
-
     protected $_defaultConfig = [
         'start' => 0,
         'length' => 10,
         'order' => [],
-        'prefixSearch' => false, // use "LIKE …%" instead of "LIKE %…%" conditions
+        'prefixSearch' => true, // use "LIKE …%" instead of "LIKE %…%" conditions
         'conditionsOr' => [],  // table-wide search conditions
         'conditionsAnd' => [], // column search conditions
         'matching' => [],      // column search conditions for foreign tables
     ];
-
     protected $_viewVars = [
         'recordsTotal' => 0,
         'recordsFiltered' => 0,
         'draw' => 0
     ];
-
     protected $_tableName = null;
-
     protected $_plugin = null;
-
     /**
      * Process draw option (pass-through)
      */
@@ -37,10 +30,8 @@ class DataTablesComponent extends Component
     {
         if (empty($this->request->query['draw']))
             return;
-
         $this->_viewVars['draw'] = (int)$this->request->query['draw'];
     }
-
     /**
      * Process query data of ajax request regarding order
      * Alters $options if delegateOrder is set
@@ -49,34 +40,23 @@ class DataTablesComponent extends Component
      */
     private function _order(array &$options)
     {
-        
-        if (!isset($this->request->query['iSortCol_0'])){
+        if (empty($this->request->query['order']))
             return;
-        }
-
-        // get the column to sort on and the sort order - copy into vars for convenience
-        $column='mDataProp_'.$this->request->query['iSortCol_0'];
-        $sortOrder=$this->request->query['sSortDir_0'];
-
         // -- add custom order
         $order = $this->config('order');
-        
-        //foreach($this->request->query['order'] as $item) {
+        foreach($this->request->query['order'] as $item) {
+            //debug($this->request->query['columns'][$item['column']]);
+            $order[$this->request->query['columns'][$item['column']]['data']] = $item['dir'];
             //$order[$this->request->query['columns'][$item['column']]['name']] = $item['dir'];
-        //}
-        $order[$this->request->query[$column]] = $sortOrder;
-
+        }
         if (!empty($options['delegateOrder'])) {
             $options['customOrder'] = $order;
         } else {
             $this->config('order', $order);
         }
-
         // -- remove default ordering as we have a custom one
         unset($options['order']);
-        
     }
-
     /**
      * Process query data of ajax request regarding filtering
      * Alters $options if delegateSearch is set
@@ -84,53 +64,42 @@ class DataTablesComponent extends Component
      * @param $options: Query options from the request
      * @return: returns true if additional filtering takes place
      */
-    private function _filter(array &$options)
+    private function _filter(array &$options) : bool
     {
-
         // -- add limit
-        if (!empty($this->request->query['iDisplayLength'])) {
-            $this->config('length', $this->request->query['iDisplayLength']);
+        if (!empty($this->request->query['length'])) {
+            $this->config('length', $this->request->query['length']);
         }
-
         // -- add offset
-        if (!empty($this->request->query['iDisplayStart'])) {
-            $this->config('start', (int)$this->request->query['iDisplayStart']);
+        if (!empty($this->request->query['start'])) {
+            $this->config('start', (int)$this->request->query['start']);
         }
-
         // -- don't support any search if columns data missing
-        if (empty($this->request->query['iColumns']))
+        if (empty($this->request->query['columns']))
             return false;
-
         // -- check table search field
-        $globalSearch = isset($this->request->query['sSearch']) ? $this->request->query['sSearch'] : false;
+        $globalSearch = $this->request->query['search']['value'] ?? false;
         if ($globalSearch && !empty($options['delegateSearch'])) {
             $options['globalSearch'] = $globalSearch;
             return true; // TODO: support for deferred local search
         }
-
         // -- add conditions for both table-wide and column search fields
         $filters = false;
-
-        for($count = 0; $count < $this->request->query['iColumns']; $count++){
- 
-            //if ($globalSearch && $column['searchable'] == 'true') {
-            if ($globalSearch && $this->request->query['bSearchable_'.$count.''] == 'true') {      
-                $this->_addCondition($this->request->query['mDataProp_'.$count.''], $globalSearch, 'or');      
+        foreach ($this->request->query['columns'] as $column) {
+            if ($globalSearch && $column['searchable'] == 'true') {
+                //$this->_addCondition($column['name'], $globalSearch, 'or');
+                $this->_addCondition($column['data'], $globalSearch, 'or');
                 $filters = true;
             }
-            $localSearch = $this->request->query['sSearch_'.$count];
-            
+            $localSearch = $column['search']['value'];
             if (!empty($localSearch)) {
-                echo "We do not use Local (column seach) for the time being so print this message and quit"; exit;
-                $this->_addCondition($this->request->query['mDataProp_'.$count.''], $this->request->query['sSearch_'.$count]);           
+                //$this->_addCondition($column['name'], $column['search']['value']);
+                $this->_addCondition($column['data'], $column['search']['value']);
                 $filters = true;
             }
         }
-
         return $filters;
-    
     }
-
     /**
      * Find data
      *
@@ -141,35 +110,27 @@ class DataTablesComponent extends Component
      */
     public function find($tableName, $finder = 'all', array $options = [])
     {
-        
         $delegateSearch = !empty($options['delegateSearch']);
-
         // -- get table object
         $table = TableRegistry::get($tableName);
         $this->_tableName = $table->alias();
-
         // -- process draw & ordering options
         $this->_draw();
         $this->_order($options);
-
         // -- call table's finder w/o filters
         $data = $table->find($finder, $options);
-
         // -- retrieve total count
         $this->_viewVars['recordsTotal'] = $data->count();
-
         // -- process filter options
         $filters = $this->_filter($options);
-
         // -- apply filters
+
         if ($filters) {
             if ($delegateSearch) {
                 // call finder again to process filters (provided in $options)
                 $data = $table->find($finder, $options);
             } else {
-                
                 $data->where($this->config('conditionsAnd'));
-                       
                 foreach ($this->config('matching') as $association => $where) {
                     $data->matching($association, function ($q) use ($where) {
                         return $q->where($where);
@@ -178,58 +139,42 @@ class DataTablesComponent extends Component
                 if (!empty($this->config('conditionsOr'))) {
                     $data->where(['or' => $this->config('conditionsOr')]);
                 }
-                
             }
         }
-
         // -- retrieve filtered count
         $this->_viewVars['recordsFiltered'] = $data->count();
-
         // -- add limit
         if ($this->config('length') > 0) { // dt might provide -1
             $data->limit($this->config('length'));
             $data->offset($this->config('start'));
         }
-
         // -- sort
         $data->order($this->config('order'));
-
         // -- set all view vars to view and serialize array
         $this->_setViewVars();
         return $data;
-
     }
-
     private function _setViewVars()
     {
         $controller = $this->_registry->getController();
-
-        $_serialize = isset($controller->viewVars['_serialize']) ? $controller->viewVars['_serialize'] : [];
+        $_serialize = $controller->viewVars['_serialize'] ?? [];
         $_serialize = array_merge($_serialize, array_keys($this->_viewVars));
-
         $controller->set($this->_viewVars);
         $controller->set('_serialize', $_serialize);
     }
-
     private function _addCondition($column, $value, $type = 'and')
     {
         $right = $this->config('prefixSearch') ? "{$value}%" : "%{$value}%";
         $condition = ["{$column} LIKE" => $right];
-        
-        //debug($this->_tableName.' :: '.$type);
-
         if ($type === 'or') {
             $this->config('conditionsOr', $condition); // merges
             return;
         }
-
-        // We only get here if we are NOT processing an 'OR' condition, eg conditions is an AND
         list($association, $field) = explode('.', $column);
-        
         if ($this->_tableName == $association) {
             $this->config('conditionsAnd', $condition); // merges
         } else {
-            $this->config('matching', [$association => $condition]);      
+            $this->config('matching', [$association => $condition]); // merges
         }
     }
 }
